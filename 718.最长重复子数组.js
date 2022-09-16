dcaeffefd8f62eeea1d377fcec73b85222be9b15
chizhen <chizhen@dm-ai.cn>
2022-09-16 18:49:00 +0800

    动态规划

--- 718.最长重复子数组.js
+++ 718.最长重复子数组.js
@@ -11,7 +11,22 @@
  * @return {number}
  */
 var findLength = function (nums1, nums2) {
-
+  const dp = []
+  for (let i = 0; i < nums1.length; i++) {
+    dp[i] = new Array(nums2.length).fill(0)
+  }
+  let max = 0
+  dp[0][0] = nums1[0] == nums2[0] ? 1 : 0
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] == nums2[j]) dp[i][j] = (i > 0 && j > 0) ? (dp[i - 1][j - 1] + 1) : 1
+      max = Math.max(max, dp[i][j])
+    }
+  }
+  // console.log(dp)
+  return max
 };
 // @lc code=end
+
+console.log(findLength([1, 2, 3, 2, 1], [3, 2, 1, 4, 7]))
 
